bfe77c7f18a891704f6d9434a7d675012b6dd0ff
X <macbookair@MacBook-Air-MacBook-9.local>
2025-12-18 14:33:53 +0500

    Initial Django project for Render

--- timepiece_site/urls.py
+++ timepiece_site/urls.py
@@ -18,5 +18,4 @@ urlpatterns = [
     path("", include("catalog.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
